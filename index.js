d91c34640870ae6520a9417f8c388d0fa9f4a86a
as30098 <alexspencer421@gmail.com>
2021-01-10 21:14:22 -0500

    commit tree d3e3a6271c2ca9ff2d08ab295d5713305e9ab2e2

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  // convert string to number
+  let time = parseInt(string);
+  if (time < 12) {
+    return "Good Morning";
+  } else if (time > 17) {
+    return "Good Evening";
+  } else {
+    return "Good Afternoon";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(greeting) {
+  document.getElementById("greeting").innerText = greeting;
+}
